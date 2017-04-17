e3bf179d405c41d23f24779c40a0a8eab2d26a5b
clovisf <clovisf@gmail.com>
2017-04-16 22:27:38 -0300

    segundo commit
    Comentários no sketch foram modificados para melhor entendimento

--- Seguidor-de-Linha-Com-Calibracao/Seguidor-de-Linha-Com-Calibracao.ino
+++ Seguidor-de-Linha-Com-Calibracao/Seguidor-de-Linha-Com-Calibracao.ino
@@ -1,6 +1,16 @@
-//Programa : Controle 2 motores DC usando Ponte H L298N
-//Autor : FILIPEFLOP
- 
+/* 
+ *  Robô seguidor de linha com Arduino UNO (Fritzen UM) 
+ *  - Controle ON-OFF dos dois motores CC  
+ *  - Utilizando apenas um sensor infravermelho (segue sempre á esquerda da linha)
+ *  
+ *  Desenvolvido por Clóvis Fritzen em 16/04/2017, para o 
+ *  blog FritzenLab .. http://FritzenLab.com.br
+ *  
+ *  Baseado no código "Controle 2 motores DC usando Ponte H L298N" 
+do autor FILIPEFLOP
+
+*/
+
 //Definicoes pinos Arduino ligados a entrada da Ponte H
 int IN1 = 3;
 int IN2 = 5;
@@ -80,7 +90,5 @@ void loop()
     analogWrite(IN3, 125); // RIGHT wheel stopped
     analogWrite(IN4, 125);
  }
- //delay(50);
- 
- 
+  
 }
